f82185e1dd4b3497e4a30b4653a1dce404ee9a5c
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-03-07 23:51:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-//your JS code here. If required.
-let p = document.getElementById("status");
-// let btn =document.getElementById("enterBtn");
-function myFun()  {
-p.innerHTML=" <h2>Entered Metaverse</h2>";
-}
-document.getElementById("enterBtn").addEventListener("click", myFun);
+document.getElementById('enterBtn').addEventListener('click', function() {
+    let statusParagraph = document.getElementById('status');
+    statusParagraph.innerHTML = '<h1>Entered Metaverse</h1>';
+});
+
